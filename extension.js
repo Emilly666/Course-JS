49ab7b2e4f72cab610bd9578ae5dfbaa031b1b60
Emilia Kalańska <kalanskae@gmail.com>
2024-04-11 14:40:03 +0200

    add extension to grid

---
+++ extension.js
@@ -0,0 +1,49 @@
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("save-tab-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+let myLeads = JSON.parse(localStorage.getItem("myLeads"))
+if (!myLeads) {
+    myLeads = []
+}
+renderList(myLeads, ulEl)
+
+function renderList(array, element) {
+    let listItems = ""
+    array.forEach(item => {
+        listItems += `
+        <li>
+            <a href='${item}' target='_blank'>
+                 ${item}
+            </a>
+        </li>`
+        
+    });
+    element.innerHTML = listItems
+}
+
+inputBtn.addEventListener("click", function () {
+    if (!inputEl.value) {
+        return
+    }
+    myLeads.push(inputEl.value) 
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    renderList(myLeads, ulEl)
+})
+
+tabBtn.addEventListener("click", function () {
+    myLeads.push(window.location.href)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderList(myLeads, ulEl)
+})
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+
+    renderList(myLeads, ulEl)
+})
