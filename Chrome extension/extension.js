535449af5a154ed4463e6f98ec5f7f24257c396d
Emilia Kalańska <kalanskae@gmail.com>
2024-04-11 14:32:53 +0200

    finnished chrome extension

--- Chrome extension/extension.js
+++ Chrome extension/extension.js
@@ -1,26 +1,51 @@
-const saveEl = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("save-tab-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
-let myLeads = []
+let myLeads = JSON.parse(localStorage.getItem("myLeads"))
+if (!myLeads) {
+    myLeads = []
+}
+renderList(myLeads, ulEl)
 
-saveEl.addEventListener("click", function () {
-    myLeads.push(inputEl.value) 
-    renderLeads()
-    inputEl.value = ""
-})
-
-function renderLeads() {
+function renderList(array, element) {
     let listItems = ""
-    myLeads.forEach(element => {
+    array.forEach(item => {
         listItems += `
         <li>
-            <a href='${element}' target='_blank'>
-                 ${element}
+            <a href='${item}' target='_blank'>
+                 ${item}
             </a>
         </li>`
         
     });
-    ulEl.innerHTML = listItems
+    element.innerHTML = listItems
 }
 
+inputBtn.addEventListener("click", function () {
+    if (!inputEl.value) {
+        return
+    }
+    myLeads.push(inputEl.value) 
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    renderList(myLeads, ulEl)
+})
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderList(myLeads, ulEl)
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+
+    renderList(myLeads, ulEl)
+})
